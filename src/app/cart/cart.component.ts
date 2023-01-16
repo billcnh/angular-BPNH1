958dd02483e6c9f828df1bb76cb1a285f8190f67
billcnh <bpelchat@comcast.net>
2023-01-15 21:48:45 -0500

    2 files modified

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -25,7 +25,7 @@ export class CartComponent {
   onSubmit(): void {
     // Process checkout data here
     this.items = this.cartService.clearCart();
-    console.warn('Your order has been submitted', this.checkoutForm.value);
+    console.warn('Success, Your order has been submitted', this.checkoutForm.value);
     this.checkoutForm.reset();
   }
 }
